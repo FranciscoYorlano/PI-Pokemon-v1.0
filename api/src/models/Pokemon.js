835cd5038a1abe471af7cac3a86990634aacc8d9
Francisco Yorlano <francisco.yorlano.arg@gmail.com>
2023-04-11 10:08:25 -0300

    Pokemon model

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,64 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
+
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('pokemon', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+    // defino el modelo
+    sequelize.define("pokemon", {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING(40),
+            allowNull: false,
+        },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isUrl: true,
+            },
+        },
+        life: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 0,
+            },
+        },
+        attack: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 0,
+            },
+        },
+        defense: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 0,
+            },
+        },
+        speed: {
+            type: DataTypes.INTEGER,
+            validate: {
+                min: 0,
+            },
+        },
+        height: {
+            type: DataTypes.INTEGER,
+            validate: {
+                min: 0,
+            },
+        },
+        weight: {
+            type: DataTypes.INTEGER,
+            validate: {
+                min: 0,
+            },
+        },
+    });
 };
