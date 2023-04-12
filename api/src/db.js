f6164000d5896792aaebaa0af6a3219691d0890e
Francisco Yorlano <francisco.yorlano.arg@gmail.com>
2023-04-12 08:27:48 -0300

    db relations ok

--- api/src/db.js
+++ api/src/db.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 const { Sequelize } = require("sequelize");
 
 // ======================== Models requires
@@ -7,10 +6,12 @@ const pokemonDefiner = require("./models/Pokemon");
 const typeDefiner = require("./models/Type");
 
 // ======================== Sequelize
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+
 const sequelize = new Sequelize(
     `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
     {
-        logging: true, // set to console.log to see the raw SQL queries
+        logging: true,
         native: false, // lets Sequelize know we can use pg-native for ~30% more speed
     }
 );
@@ -21,7 +22,8 @@ typeDefiner(sequelize);
 const { Pokemon, Type } = sequelize.models;
 
 // ======================== Models relations
-// Product.hasMany(Reviews);
+Pokemon.belongsToMany(Type, { through: "PokemonsTypes" });
+Type.belongsToMany(Pokemon, { through: "PokemonsTypes" });
 
 module.exports = {
     ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
